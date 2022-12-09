26aee18286b994304aab93cfc5492acd581cc289
Chinh2139 <chinhvu1194@gmail.com>
2022-12-08 19:21:11 -0800

    bài tập

--- SQL/Testing_System_Assignment_3.sql
+++ SQL/Testing_System_Assignment_3.sql
@@ -43,44 +43,44 @@ VALUE 	('Dev'),
 CREATE TABLE AccountID (
 Account_ID INT AUTO_INCREMENT,
 Email VARCHAR (255),
-User_name VARCHAR (255),
+User_Name VARCHAR (255),
 Department_ID TINYINT,
 Position_ID TINYINT,
-Createdate DATE,
+Create_Date DATE,
 PRIMARY KEY (Account_ID),
 FOREIGN KEY  (Position_ID) REFERENCES PositionID (Position_ID),
 FOREIGN KEY  (Department_ID) REFERENCES Department (Department_ID)
 );
 -- add database table 3
-INSERT INTO AccountID (Email, User_name,Department_ID,Position_ID, Createdate )
+INSERT INTO AccountID (Email, User_Name,Department_ID,Position_ID, Create_Date )
 VALUE 	('antony_ya@yahoo.com', 	'antony',		'1',		'1', 	'2008-10-11'),
-		('annabellla@yahoo.com', 	'annablla',		'5',		'2', 	'2008-9-11'),
-		('betovaloti@yahoo.com', 	'betovloti',	'3',		'3', 	'2008-7-16'),
+		('annabellla@yahoo.com', 	'annaasfblla',	'5',		'2', 	'2008-9-11'),
+		('betovaloti@yahoo.com', 	'betoverloti',	'3',		'3', 	'2008-7-16'),
 		('vatkahteo@yahoo.com',	 	'fjjj',			'3',		'4', 	'2008-11-15'),
         ('asffteo@yahoo.com', 		'dfdsgd',		'2',		'5', 	'2008-11-15'),
         ('vatasjfho@yahoo.com', 	'wtwww',		'2',		'4', 	'2008-11-15'),
         ('basfsgaggteo@yahoo.com', 	'vatkeo',		'5',		'3', 	'2008-11-15'),
         ('sdffgf@yahoo.com', 		'asffsdg',		'1',		'1',	'2008-11-15'),
-        ('asfsdgweg@yahoo.com', 	'wetwtqt',		'2',		'4',	'2008-11-15'),
-        ('afafas@yahoo.com', 		'sdsdg',		'3',		'2', 	'2008-11-15'),
-		('asfkjbiuasf@yahoo.com', 	'asfbiuasf',	'4',		'5', 	'2008-1-18');
+        ('asfsdgweg@yahoo.com', 	'wetwasftqt',	'2',		'4',	'2008-11-15'),
+        ('afafas@yahoo.com', 		'sdsdasfg',		'3',		'2', 	'2008-11-15'),
+		('asfkjbiuasf@yahoo.com', 	'asfbiqwruasf',	'4',		'5', 	'2008-1-18');
 -- table 4
 CREATE TABLE GroupID (
 Group_ID SMALLINT AUTO_INCREMENT,
 Group_name VARCHAR (255),
 Creator_ID TINYINT,
-Create_date DATE,
+Create_Date DATE,
 PRIMARY KEY (Group_ID)
 );
 -- add database table 4
-INSERT INTO GroupID (Group_ID, Group_name,Creator_ID,Create_date)
+INSERT INTO GroupID (Group_ID, Group_name,Creator_ID,Create_Date)
 VALUE 	('256', 	'railway60',	'1', 		'2022-12-08'),
 		('542', 	'railway61',	'2', 		'2022-12-18'),
         ('552', 	'railway62',	'7', 		'2022-12-11'),
-        ('365', 	'railway64',	'6', 		'2022-12-25'),
+        ('365', 	'railway64',	'6', 		'2018-12-25'),
         ('395', 	'railway65',	'4', 		'2022-12-21'),
         ('375', 	'railway66',	'6', 		'2022-12-25'),
-        ('265', 	'railway67',	'8', 		'2022-12-05'),
+        ('265', 	'railway67',	'8', 		'2018-12-05'),
         ('364', 	'railway68',	'10', 		'2022-12-15'),
         ('195', 	'railway69',	'6', 		'2022-12-29'),
         ('584', 	'railway70',	'3', 		'2022-12-30');
@@ -208,15 +208,15 @@ PRIMARY KEY (Exam_ID)
 );
 -- add database table 10
 INSERT INTO Exam (Code_ID, Title,Category_ID,Duration,Creator_ID,CreateDate)
-VALUE 	('257',		'tiêu đề',		'1',	'90 phút',	'2435',		'2022-11-29'),
-		('157',		'tiêu đề',		'5',	'90 phút',	'2135',		'2022-11-29'),
-        ('287',		'tiêu đề',		'4',	'60 phút',	'2485',		'2022-11-29'),
-        ('227',		'tiêu đề',		'3',	'90 phút',	'2435',		'2022-11-29'),
-        ('657',		'tiêu đề',		'2',	'60 phút',	'2435',		'2022-11-29'),
-        ('258',		'tiêu đề',		'1',	'90 phút',	'2435',		'2022-11-29'),
-        ('298',		'tiêu đề',		'8',	'60 phút',	'2485',		'2022-11-29'),
-        ('217',		'tiêu đề',		'9',	'60 phút',	'2445',		'2022-11-29'),
-        ('253',		'tiêu đề',		'1',	'90 phút',	'2435',		'2022-11-29');
+VALUE 	('257',		'tiêu đề',		'1',	'90',	'2435',		'2022-11-29'),
+		('157',		'tiêu đề',		'5',	'90',	'2135',		'2022-11-29'),
+        ('287',		'tiêu đề',		'4',	'60',	'2485',		'2017-11-29'),
+        ('227',		'tiêu đề',		'3',	'90',	'2435',		'2022-11-29'),
+        ('657',		'tiêu đề',		'2',	'60',	'2435',		'2015-11-29'),
+        ('258',		'tiêu đề',		'1',	'90',	'2435',		'2022-11-29'),
+        ('298',		'tiêu đề',		'8',	'60',	'2485',		'2022-11-29'),
+        ('217',		'tiêu đề',		'9',	'60',	'2445',		'2022-11-29'),
+        ('253',		'tiêu đề',		'1',	'90',	'2435',		'2022-11-29');
 -- table 11
 CREATE TABLE ExamQuestion (
 Exam_ID TINYINT,
@@ -250,5 +250,63 @@ SELECT * FROM AccountID WHERE Department_ID IN (1,2,3,4,5);
 SELECT * FROM AccountID WHERE Department_ID BETWEEN 1 AND 5;
 
 
+-- ================================="Testing_System_Assignment_3"==================
+-- Question 2:
+SELECT * FROM Department;
 
+-- Question 3:
+SELECT AccountID FROM Department 
+WHERE Department_name = 'Sale';
 
+-- Question 4:
+SELECT * FROM fresher.accountid 
+WHERE length(User_Name) =(SELECT max(length(User_Name)) FROM fresher.accountid);
+-- Question 5:
+SELECT * FROM fresher.accountid 
+WHERE length(User_Name) =(SELECT max((User_Name)) FROM fresher.accountid) 
+AND Department_ID = 3;
+
+-- Question 6:
+
+SELECT Group_Name FROM GroupID 
+WHERE Create_date <='2019-12-20';
+
+-- Question 7:
+SELECT Question_ID FROM Question 
+WHERE Question_ID >= 4;
+
+-- Question 8:
+SELECT Code_ID FROM Exam 
+WHERE Duration >= 60 AND CreateDate <='2019-12-20';
+
+-- Question 9:
+SELECT * FROM  fresher.groupid 
+ORDER BY Create_Date DESC
+LIMIT 5;
+-- Question 10:
+
+SELECT count('nhân viên') FROM Department 
+WHERE Department_ID =2;
+
+-- Question 11:
+SELECT User_Name FROM fresher.accountid 
+WHERE User_Name LIKE 'D%o';
+
+-- Question 12:
+DELETE FROM fresher.exam 
+WHERE CreateDate < '2019-12-20';
+
+-- Question 13:
+DELETE FROM fresher.question 
+WHERE Content LIKE 'câu hỏi%';
+
+-- Question 14:
+UPDATE 	fresher.accountid
+SET 	User_Name = 'Nguyễn Bá Lộc',
+		Email = 'loc.nguyenba@vti.com.vn'
+ WHERE Account_ID = 5;       
+
+-- Question 15:
+UPDATE fresher.accountid
+SET Account_ID = 5
+WHERE Group_ID = 195;
